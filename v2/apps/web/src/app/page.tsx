ac911991bc86b2aa5d5939f33e516a3e1345d110
Elliot <elliot@mozstro.com>
2025-03-30 02:09:53 +0100

    log

--- v2/apps/web/src/app/page.tsx
+++ v2/apps/web/src/app/page.tsx
@@ -301,6 +301,7 @@ export default function Home() {
         </div>
       </section>
 
+
       {/* Team Section */}
       <section ref={teamRef} className="py-16 bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
